57cd429af42cb2d7487bc9c10cbb63621ed9329f
Devansh Bhavsar <devanshbhavsar2006@gmail.com>
2025-08-07 21:05:28 +0530

    fix: lint action dependency bug

--- web/app/actions/website.ts
+++ web/app/actions/website.ts
@@ -8,128 +8,130 @@ import { redirect } from 'next/navigation'
 import { revalidatePath } from 'next/cache'
 
 export async function createWebsite(_: unknown, formData: FormData) {
-    const user = await auth()
+  const user = await auth()
 
-    if (!user?.token) {
-        redirect('/login')
+  if (!user?.token) {
+    redirect('/login')
+  }
+
+  const parsedData = websiteSchema.safeParse({
+    url: formData.get('url'),
+    frequency: formData.get('frequency'),
+    regions: formData.getAll('regions') as string[],
+  })
+
+  if (!parsedData.success) {
+    return {
+      errors: parsedData.error.flatten().fieldErrors,
     }
+  }
 
-    const parsedData = websiteSchema.safeParse({
-        url: formData.get('url'),
-        frequency: formData.get('frequency'),
-        regions: formData.getAll('regions') as string[],
+  try {
+    await axios.post(`${API_URL}/website`, parsedData.data, {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
     })
 
-    if (!parsedData.success) {
-        return {
-            errors: parsedData.error.flatten().fieldErrors,
-        }
+    revalidatePath('/dashboard')
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return {
+        error:
+          error.response?.data?.error ||
+          'An error occurred while creating the website.',
+      }
     }
 
-    try {
-        await axios.post(`${API_URL}/website`, parsedData.data, {
-            headers: {
-                Authorization: `Bearer ${user.token}`,
-            },
-        })
-
-        revalidatePath('/dashboard')
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            return {
-                error:
-                    error.response?.data?.error ||
-                    'An error occurred while creating the website.',
-            }
-        }
-
-        return {
-            error: 'An unexpected error occurred while creating the website.',
-        }
+    return {
+      error: 'An unexpected error occurred while creating the website.',
     }
+  }
 }
 
 export async function deleteWebsite(id: string) {
-    const user = await auth()
+  const user = await auth()
 
-    if (!user?.token) {
-        redirect('/login')
-    }
+  if (!user?.token) {
+    redirect('/login')
+  }
 
-    try {
-        await axios.delete(`${API_URL}/website/${id}`, {
-            headers: {
-                Authorization: `Bearer ${user.token}`,
-            },
-        })
+  try {
+    await axios.delete(`${API_URL}/website/${id}`, {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    })
 
-        revalidatePath('/dashboard')
-    } catch (error) {}
+    revalidatePath('/dashboard')
+  } catch (error) {
+    console.error('Error deleting website:', error)
+  }
 }
 
 export async function editWebsite(websiteId: string, formData: FormData) {
-    const user = await auth()
+  const user = await auth()
 
-    if (!user?.token) {
-        redirect('/login')
+  if (!user?.token) {
+    redirect('/login')
+  }
+
+  const parsedData = websiteSchema.safeParse({
+    url: formData.get('url'),
+    frequency: formData.get('frequency'),
+    regions: formData.getAll('regions') as string[],
+  })
+
+  if (!parsedData.success) {
+    return {
+      errors: parsedData.error.flatten().fieldErrors,
     }
+  }
 
-    const parsedData = websiteSchema.safeParse({
-        url: formData.get('url'),
-        frequency: formData.get('frequency'),
-        regions: formData.getAll('regions') as string[],
+  try {
+    await axios.put(`${API_URL}/website/${websiteId}`, parsedData.data, {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
     })
 
-    if (!parsedData.success) {
-        return {
-            errors: parsedData.error.flatten().fieldErrors,
-        }
+    revalidatePath('/dashboard')
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return {
+        error:
+          error.response?.data?.error ||
+          'An error occurred while editing the website.',
+      }
     }
 
-    try {
-        await axios.put(`${API_URL}/website/${websiteId}`, parsedData.data, {
-            headers: {
-                Authorization: `Bearer ${user.token}`,
-            },
-        })
-
-        revalidatePath('/dashboard')
-    } catch (error) {
-        if (error instanceof AxiosError) {
-            return {
-                error:
-                    error.response?.data?.error ||
-                    'An error occurred while editing the website.',
-            }
-        }
-
-        return {
-            error: 'An unexpected error occurred while editing the website.',
-        }
+    return {
+      error: 'An unexpected error occurred while editing the website.',
     }
+  }
 }
 
 export async function pingWebsite(_: unknown, url: string) {
-    try {
-        await axios.head(url, {
-            withCredentials: false,
-            timeout: 5000,
-        })
+  try {
+    await axios.head(url, {
+      withCredentials: false,
+      timeout: 5000,
+    })
 
-        return {
-            status: true,
-        }
-    } catch (e) {
-        if (e instanceof AxiosError) {
-            return {
-                status: false,
-                error: e.response?.statusText || 'Failed to ping the website.',
-            }
-        }
-
-        return {
-            status: false,
-            error: 'An unexpected error occurred while pinging the website.',
-        }
+    return {
+      status: true,
     }
+  } catch (e) {
+    if (e instanceof AxiosError) {
+      return {
+        status: false,
+        error: e.response?.statusText || 'Failed to ping the website.',
+      }
+    }
+
+    return {
+      status: false,
+      error: 'An unexpected error occurred while pinging the website.',
+    }
+  }
 }
--- web/components/dashboard/dialog.tsx
+++ web/components/dashboard/dialog.tsx
@@ -3,13 +3,13 @@
 import { Label } from '../ui/label'
 import { Button } from '../ui/button'
 import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
 } from '../ui/dialog'
 import { Input } from '../ui/input'
 import { Checkbox } from '../ui/checkbox'
@@ -17,11 +17,11 @@ import ReactCountryFlag from 'react-country-flag'
 import { pingWebsite } from '@/app/actions/website'
 import { startTransition, useActionState, useEffect, useState } from 'react'
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from '../ui/select'
 import { useDebounce } from '@/hooks/use-debounce'
 import { CircleCheckBig, CircleX, LoaderCircle } from 'lucide-react'
@@ -29,203 +29,204 @@ import { Monitors } from '@/app/dashboard/data-table'
 import { fetchRegions } from '@/app/actions/region'
 
 type Region = {
-    id: string
-    name: string
+  id: string
+  name: string
 }
 
 const frequencies = [
-    { value: '30s', label: '30 Seconds' },
-    { value: '1m', label: '1 Minute' },
-    { value: '3m', label: '3 Minutes' },
-    { value: '5m', label: '5 Minutes' },
+  { value: '30s', label: '30 Seconds' },
+  { value: '1m', label: '1 Minute' },
+  { value: '3m', label: '3 Minutes' },
+  { value: '5m', label: '5 Minutes' },
 ]
 
 interface DialogProps {
-    label: string
-    description?: string
-    data?: Monitors
-    onSubmitAction: (_: unknown, formData: FormData) => Promise<any>
-    children?: React.ReactNode
+  label: string
+  description?: string
+  data?: Monitors
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onSubmitAction: (_: unknown, formData: FormData) => Promise<any>
+  children?: React.ReactNode
 }
 
 export function DialogBox({
-    label,
-    description,
-    data,
-    onSubmitAction,
-    children,
+  label,
+  description,
+  data,
+  onSubmitAction,
+  children,
 }: DialogProps) {
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    const [state, action, pending] = useActionState(onSubmitAction, null)
-    const [urlState, urlAction, urlPending] = useActionState(pingWebsite, null)
-    const [regionState, regionAction, regionPending] = useActionState(
-        fetchRegions,
-        null,
-    )
+  const [state, action, pending] = useActionState(onSubmitAction, null)
+  const [urlState, urlAction, urlPending] = useActionState(pingWebsite, null)
+  const [regionState, regionAction, regionPending] = useActionState(
+    fetchRegions,
+    null,
+  )
 
-    useEffect(() => {
-        if (open) {
-            startTransition(() => {
-                regionAction()
-            })
-        }
-    }, [open])
+  useEffect(() => {
+    if (open) {
+      startTransition(() => {
+        regionAction()
+      })
+    }
+  }, [open, regionAction])
 
-    const [url, setUrl] = useState(data?.url || 'https://')
-    const debouncedUrl = useDebounce(url, 500)
+  const [url, setUrl] = useState(data?.url || 'https://')
+  const debouncedUrl = useDebounce(url, 500)
 
-    useEffect(() => {
-        if (debouncedUrl && debouncedUrl !== 'https://') {
-            startTransition(() => {
-                urlAction(debouncedUrl)
-            })
-        }
-    }, [debouncedUrl, urlAction])
+  useEffect(() => {
+    if (debouncedUrl && debouncedUrl !== 'https://') {
+      startTransition(() => {
+        urlAction(debouncedUrl)
+      })
+    }
+  }, [debouncedUrl, urlAction])
 
-    return (
-        <Dialog open={open} onOpenChange={setOpen}>
-            {children}
-            <DialogContent>
-                <DialogHeader className="font-sans">
-                    <DialogTitle>{label}</DialogTitle>
-                    <DialogDescription>{description}</DialogDescription>
-                </DialogHeader>
-                <form action={action} className="grid gap-6">
-                    <div className="grid gap-6">
-                        <div className="grid gap-3">
-                            <Label htmlFor="url">Url</Label>
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      {children}
+      <DialogContent>
+        <DialogHeader className="font-sans">
+          <DialogTitle>{label}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <form action={action} className="grid gap-6">
+          <div className="grid gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="url">Url</Label>
 
-                            <div className="relative flex items-center">
-                                <Input
-                                    id="url"
-                                    type="text"
-                                    name="url"
-                                    value={url}
-                                    placeholder="https://example.com"
-                                    required
-                                    onChange={(e) => {
-                                        setUrl(e.target.value)
-                                    }}
-                                />
+              <div className="relative flex items-center">
+                <Input
+                  id="url"
+                  type="text"
+                  name="url"
+                  value={url}
+                  placeholder="https://example.com"
+                  required
+                  onChange={(e) => {
+                    setUrl(e.target.value)
+                  }}
+                />
 
-                                {url && url.length > 'https://'.length ? (
-                                    <span className="absolute right-3">
-                                        {urlPending ? (
-                                            <LoaderCircle
-                                                size={18}
-                                                className="animate-spin opacity-50"
-                                            />
-                                        ) : urlState?.status ? (
-                                            <CircleCheckBig
-                                                size={18}
-                                                className="text-green-500"
-                                            />
-                                        ) : (
-                                            <CircleX
-                                                size={18}
-                                                className="text-red-500"
-                                            />
-                                        )}
-                                    </span>
-                                ) : null}
-                            </div>
-                            {state?.errors?.url && (
-                                <p className="text-muted-foreground font-sans text-sm">
-                                    {state.errors.url}
-                                </p>
-                            )}
+                {url && url.length > 'https://'.length ? (
+                  <span className="absolute right-3">
+                    {urlPending ? (
+                      <LoaderCircle
+                        size={18}
+                        className="animate-spin opacity-50"
+                      />
+                    ) : urlState?.status ? (
+                      <CircleCheckBig
+                        size={18}
+                        className="text-green-500"
+                      />
+                    ) : (
+                      <CircleX
+                        size={18}
+                        className="text-red-500"
+                      />
+                    )}
+                  </span>
+                ) : null}
+              </div>
+              {state?.errors?.url && (
+                <p className="text-muted-foreground font-sans text-sm">
+                  {state.errors.url}
+                </p>
+              )}
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="frequencies">Frequency</Label>
+              <Select
+                defaultValue={(data && data?.frequency) || '3m'}
+                name="frequency"
+              >
+                <SelectTrigger className="w-50">
+                  <SelectValue placeholder="Select frequency" />
+                </SelectTrigger>
+                <SelectContent>
+                  {frequencies.map((frequency) => (
+                    <SelectItem
+                      key={frequency.value}
+                      value={frequency.value}
+                    >
+                      {frequency.label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid gap-3">
+              {regionPending ? (
+                <LoaderCircle
+                  size={18}
+                  className="animate-spin opacity-50"
+                />
+              ) : regionState?.regions &&
+                regionState.regions.length > 0 ? (
+                <div className="grid gap-3">
+                  <Label htmlFor="regions">Regions</Label>
+                  <div className="flex flex-wrap gap-3">
+                    {regionState.regions.map(
+                      (region: Region) => (
+                        <div
+                          key={region.id}
+                          className="flex items-start gap-3"
+                        >
+                          <Checkbox
+                            name="regions"
+                            value={region.name}
+                            defaultChecked={
+                              data &&
+                                data.regions.find(
+                                  (r) =>
+                                    r ==
+                                    region.name,
+                                )
+                                ? true
+                                : false
+                            }
+                          />
+                          <ReactCountryFlag
+                            countryCode={region.name.toUpperCase()}
+                            svg
+                          />
                         </div>
-                        <div className="grid gap-3">
-                            <Label htmlFor="frequencies">Frequency</Label>
-                            <Select
-                                defaultValue={(data && data?.frequency) || '3m'}
-                                name="frequency"
-                            >
-                                <SelectTrigger className="w-50">
-                                    <SelectValue placeholder="Select frequency" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {frequencies.map((frequency) => (
-                                        <SelectItem
-                                            key={frequency.value}
-                                            value={frequency.value}
-                                        >
-                                            {frequency.label}
-                                        </SelectItem>
-                                    ))}
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <div className="grid gap-3">
-                            {regionPending ? (
-                                <LoaderCircle
-                                    size={18}
-                                    className="animate-spin opacity-50"
-                                />
-                            ) : regionState?.regions &&
-                              regionState.regions.length > 0 ? (
-                                <div className="grid gap-3">
-                                    <Label htmlFor="regions">Regions</Label>
-                                    <div className="flex flex-wrap gap-3">
-                                        {regionState.regions.map(
-                                            (region: Region) => (
-                                                <div
-                                                    key={region.id}
-                                                    className="flex items-start gap-3"
-                                                >
-                                                    <Checkbox
-                                                        name="regions"
-                                                        value={region.name}
-                                                        defaultChecked={
-                                                            data &&
-                                                            data.regions.find(
-                                                                (r) =>
-                                                                    r ==
-                                                                    region.name,
-                                                            )
-                                                                ? true
-                                                                : false
-                                                        }
-                                                    />
-                                                    <ReactCountryFlag
-                                                        countryCode={region.name.toUpperCase()}
-                                                        svg
-                                                    />
-                                                </div>
-                                            ),
-                                        )}
-                                    </div>
-                                </div>
-                            ) : (
-                                <p className="text-muted-foreground font-sans text-sm">
-                                    No regions available.
-                                </p>
-                            )}
-                            {state?.errors?.regions && (
-                                <p className="text-muted-foreground font-sans text-sm">
-                                    {state.errors.regions}
-                                </p>
-                            )}
-                        </div>
-                        {state?.error && (
-                            <p className="text-muted-foreground font-sans text-sm">
-                                {state.error}
-                            </p>
-                        )}
-                    </div>
-                    <DialogFooter className="sm:justify-start">
-                        <Button type="submit" disabled={pending}>
-                            {label}
-                        </Button>
-                        <DialogClose asChild>
-                            <Button variant="outline" disabled={pending}>
-                                Cancel
-                            </Button>
-                        </DialogClose>
-                    </DialogFooter>
-                </form>
-            </DialogContent>
-        </Dialog>
-    )
+                      ),
+                    )}
+                  </div>
+                </div>
+              ) : (
+                <p className="text-muted-foreground font-sans text-sm">
+                  No regions available.
+                </p>
+              )}
+              {state?.errors?.regions && (
+                <p className="text-muted-foreground font-sans text-sm">
+                  {state.errors.regions}
+                </p>
+              )}
+            </div>
+            {state?.error && (
+              <p className="text-muted-foreground font-sans text-sm">
+                {state.error}
+              </p>
+            )}
+          </div>
+          <DialogFooter className="sm:justify-start">
+            <Button type="submit" disabled={pending}>
+              {label}
+            </Button>
+            <DialogClose asChild>
+              <Button variant="outline" disabled={pending}>
+                Cancel
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
 }
